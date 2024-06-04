904829408d0f9e49e6e7d04ab7e3853f342b6a72
Initdd <joaomariagoncalves@sapo.pt>
2024-06-04 23:11:59 +0100

    Amend last commit

--- app/src/main/java/com/example/passmanager/dal/mapper/CredentialMapper.kt
+++ app/src/main/java/com/example/passmanager/dal/mapper/CredentialMapper.kt
@@ -1,7 +1,7 @@
 package com.example.passmanager.dal.mapper
 
-import com.example.passmanager.dal.dto.CredentialDT
 import com.example.passmanager.dal.domain.CredentialDO
+import com.example.passmanager.dal.dto.CredentialDT
 
 object CredentialMapper {
     fun toDomain(dto: CredentialDT): CredentialDO {
