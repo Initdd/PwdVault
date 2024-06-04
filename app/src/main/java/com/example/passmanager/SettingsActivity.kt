b821fb216c1f2386469161c8fba9e90139ef3733
Initdd <joaomariagoncalves@sapo.pt>
2024-06-04 23:13:40 +0100

    Added a save every time the theme is toggled. May change this later

--- app/src/main/java/com/example/passmanager/SettingsActivity.kt
+++ app/src/main/java/com/example/passmanager/SettingsActivity.kt
@@ -1,38 +1,24 @@
 package com.example.passmanager
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ButtonElevation
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Slider
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.passmanager.dal.domain.ThemeModeDO
-import com.example.passmanager.scripts.MyElevatedButton
-import com.example.passmanager.scripts.MySwitch
+import com.example.passmanager.scripts.buttons.MyElevatedButton
+import com.example.passmanager.scripts.buttons.MySwitch
 import com.example.passmanager.ui.theme.PassManagerTheme
 
 
@@ -86,6 +72,7 @@ fun SettingsPage() {
                             ThemeModeDO.LIGHT
                         }
                         themeModeManager.setTheme(themeMode.value)
+                        themeModeManager.saveTMToFile()
                     }
                 }
             }
